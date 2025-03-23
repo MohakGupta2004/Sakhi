d8f9831f23b632b3dba982bfb28362816a9095ba
MohakGupta2004 <mohakgupta500@gmail.com>
2025-03-23 19:27:01 +0530

    fix(frontend): Remaining fixes done

--- client/src/pages/FinancialHelp.jsx
+++ client/src/pages/FinancialHelp.jsx
@@ -9,17 +9,17 @@ const FinancialHelp = () => {
         </header>
   
         <section className="assistance-options">
-          <div className="card">
+          <div className="f-card">
             <h2>Emergency Funds</h2>
             <p>Apply for emergency financial aid in times of crisis.</p>
             <button className="btn">Apply Now</button>
           </div>
-          <div className="card">
+          <div className="f-card">
             <h2>Budget Planning</h2>
             <p>Learn how to create and manage your budget effectively.</p>
             <button className="btn">Get Started</button>
           </div>
-          <div className="card">
+          <div className="f-card">
             <h2>Debt Relief</h2>
             <p>Find resources to help manage and reduce your debts.</p>
             <button className="btn">Learn More</button>
