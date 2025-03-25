90cd9b7631e313346bc8904146b76407fc98bbde
Mohak Gupta <mohakgupta500@gmail.com>
2025-03-25 23:42:21 +0530

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://sakhi-black.vercel.app",
     methods: ["GET", "POST"],
   },
 });
